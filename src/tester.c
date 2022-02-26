75b88d75e575121080d8ea7817595a259e8f115b
Luigi Quattrociocchi <lquatt2@gmail.com>
2022-02-26 03:14:36 -0500

    tests

--- src/tester.c
+++ src/tester.c
@@ -1,60 +1,157 @@
 #include "ymalloc.h"
 
 #include <stdio.h>
-#include <string.h>
+
+void MallocFreeTest1() {
+    void* x = ymalloc(20);
+    void* y = ymalloc(20);
+    void* z = ymalloc(20);
+    DumpFreeList();
+    // xx yy zz FFFFFF
+
+    yfree(x);
+    DumpFreeList();
+    // FF yy zz FFFFFF
+    
+    yfree(y);
+    DumpFreeList();
+    // FFFF zz FFFFFF
+
+    yfree(z);
+    DumpFreeList();
+    // FFFFFFFFFFFF
+}
+
+void MallocFreeTest2() {
+    void* x = ymalloc(4096-16);
+    DumpFreeList();
+    // xxxxxxxxx
+
+    void* y = ymalloc(69);
+    DumpFreeList();
+    // xxxxxxxxx yy
+
+    yfree(x);
+    DumpFreeList();
+    // FFFFFFFFF yy
+
+    yfree(y);
+    DumpFreeList();
+    // FFFFFFFFFFF
+}
+
+void MallocFreeTest3() {
+    void* x = ymalloc(20);
+    DumpFreeList();
+    // xx FFFFFFFFF
+
+    void* y = ymalloc(5000);
+    DumpFreeList();
+    // xx yyyyyyyy FFFFFFFFF
+    
+    void* z = ymalloc(20);
+    DumpFreeList();
+    // xx yyyyyyyy zz FFFFFFF
+
+    void* w = ymalloc(20);
+    DumpFreeList();
+    // xx yyyyyyyy zz ww FFFFF
+
+    yfree(z);
+    DumpFreeList();
+    // xx yyyyyyyy FF ww FFFFF
+
+    yfree(w);
+    DumpFreeList();
+    // xx yyyyyyyy FFFFFFFFF
+
+    yfree(x);
+    DumpFreeList();
+    // FF yyyyyyyy FFFFFFFFF
+
+    yfree(y);
+    DumpFreeList();
+    // FFFFFFFFFFFFFFFFFFF
+}
+
+void MallocFreeTest4() {
+    void* a = ymalloc(20);
+    void* b = ymalloc(30);
+    void* c = ymalloc(40);
+    void* d = ymalloc(50);
+    void* e = ymalloc(60);
+    void* f = ymalloc(70);
+    DumpFreeList();
+    // aa bb cc dd ee ff FFFFFFF
+
+    yfree(a);
+    yfree(c);
+    
+    DumpFreeList();
+    // FF bb FF dd ee ff FFFFFFF
+
+    yfree(b);
+    DumpFreeList();
+    // FFFFFF dd ee ff FFFFFFF
+
+    yfree(d);
+    DumpFreeList();
+    // FFFFFFFF ee ff FFFFFFF
+
+    yfree(f);
+    DumpFreeList();
+    // FFFFFFFF ee FFFFFFFFF
+
+    yfree(e);
+    DumpFreeList();
+    // FFFFFFFF ee FFFFFFFFF
+}
+
+void MallocFreeTest5() {
+    void* x = ymalloc(30);
+    void* y = ymalloc(70);
+    void* z = ymalloc(150);
+    DumpFreeList();
+    // xx yyy zzzz FFFFFF
+    
+    yfree(y);
+    DumpFreeList();
+    // xx FFF zzzz FFFFFF
+
+    void* big = ymalloc(5000);
+    DumpFreeList();
+    // xx FFF zzzz bbbbbbb FFFFFF
+
+    void* a = ymalloc(40);
+    DumpFreeList();
+    // xx aa F zzzz bbbbbbb FFFFFF
+    
+    yfree(x);
+    DumpFreeList();
+    // FF aa F zzzz bbbbbbb FFFFFF
+    
+    yfree(a);
+    DumpFreeList();
+    // FFFFF zzzz bbbbbbb FFFFFF
+
+    yfree(big);
+    DumpFreeList();
+    // FFFFF zzzz FFFFFFFFFFFFF
+
+    yfree(z);
+    DumpFreeList();
+    // FFFFFFFFFFFFFFFFFFFFFF
+}
+
+void ReallocTest1() {
+    
+}
 
 int main() {
-
-    void* x = ymalloc(30);
-    void* y = ymalloc(1);
-    void* z = ymalloc(100);
-    memset(x, 'A', 30);
-    memset(y, 'B', 1);
-    memset(z, 'C', 100);
-    yrealloc(NULL, 0);
-    
-    printf("alloc ye\n");
-    void* ye = ymalloc(5000);
-    memset(ye, 'X', 5000);
-    yrealloc(NULL, 0);
-
-    yrealloc(NULL, 0);
-    printf("free y\n");
-    yfree(y);
-    yrealloc(NULL, 0);
-    printf("free x\n");
-    yfree(x);
-    yrealloc(NULL, 0);
-
-    printf("alloc w\n");
-    void* w = ymalloc(20);
-    memset(w, 'D', 20);
-    yrealloc(NULL, 0);
-
-    printf("alloc big\n");
-    void* big = ymalloc(20000);
-    memset(big, 'X', 20000);
-    yrealloc(NULL, 0);
-
-    printf("free big\n");
-    yfree(big);
-    yrealloc(NULL, 0);
-
-
-    printf("free z\n");
-    yfree(z);
-    yrealloc(NULL, 0);
-    
-
-    printf("free w\n");
-    yfree(w);
-    yrealloc(NULL, 0);
-
-    // size_t* arr = ((size_t*) x) - 1;
-    // for (size_t i = 0; i <= (HEAP_INIT_SIZE)/sizeof(size_t); ++i)
-    //     printf("%zu: %zu (0x%zx)\n", i, arr[i], arr[i]);
-
-    // size_t i = 1 + (HEAP_INIT_SIZE)/sizeof(size_t);
-    // printf("%zu (end): %zu\n", i, arr[i]);
-
+    // MallocFreeTest1();
+    // MallocFreeTest2();
+    // MallocFreeTest3();
+    // MallocFreeTest4();
+    // MallocFreeTest5();
+    ReallocTest1();
 }
